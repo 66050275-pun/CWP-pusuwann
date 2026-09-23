2a98917bec4fa267290e0d4021fe8ba166bd5ede
myMacBoOK <mymacbook@MacBook-Pro-Ultra-10Tb.local>
2026-09-23 18:52:04 +0700

    Simplify test command comments in parameters.py

--- cell05/ex04/parameters.py
+++ cell05/ex04/parameters.py
@@ -5,5 +5,5 @@ print(f"Number of parameters: {len(sys.argv) - 1}.")
 
 # Example test commands in terminal:
 # ./cell05/ex04/parameters.py
-# ./cell05/ex04/parameters.py "initiation"
-# ./cell05/ex04/parameters.py "this" "is" "crazy" "there 's" "everywhere!"
+# ./cell05/ex04/parameters.py initiation
+# ./cell05/ex04/parameters.py this is crazy there everywhere
