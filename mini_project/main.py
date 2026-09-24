e9809c574953430f87ff1014695e69d47d55be10
myMacBoOK <mymacbook@MacBook-Pro-Ultra-10Tb.local>
2026-09-24 21:33:17 +0700

    Add Rush00 project and mini_project with checkmate and bonus

---
+++ mini_project/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+from checkmate import checkmate
+
+def main():
+    board = """\
+R...
+.K..
+..P.
+....\
+"""
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
+
+# Example test command in terminal:
+# ./rush00/ex00/main.py
