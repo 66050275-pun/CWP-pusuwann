ca8cedd879da2b697ce12a43a2f36d96f612c3d7
myMacBoOK <mymacbook@MacBook-Pro-Ultra-10Tb.local>
2026-09-24 23:04:37 +0700

    Clean code for submission in miniproject/ex00

--- miniproject/ex00/main.py
+++ miniproject/ex00/main.py
@@ -4,7 +4,6 @@ from checkmate import checkmate
 from board_chess import *
 
 def main():
-    # กระดานเริ่มต้น (สามารถเปลี่ยนกระดานทดสอบได้จากตรงนี้ หรือเรียกจาก board_chess ก็ได้)
     board = """\
 R...
 .K..
@@ -13,20 +12,15 @@ P.P.
 """
 
     try:
-        # หากมีการระบุชื่อไฟล์กระดานผ่าน Terminal (เช่น python3 main.py test.chess)
         if len(sys.argv) > 1:
             for filepath in sys.argv[1:]:
                 with open(filepath, 'r') as f:
                     file_board = f.read()
                 checkmate(file_board)
         else:
-            # รันกระดานเริ่มต้น
             checkmate(board)
     except Exception:
         pass
 
 if __name__ == "__main__":
     main()
-
-# Example test commands in terminal:
-# ./mini_project/ex00/main.py
